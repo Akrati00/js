e3b54c502777023746f639b3ae0c6777110f511e
Akrati Bhadauria <122599551+Akrati00@users.noreply.github.com>
2024-10-13 13:18:54 +0000

    .

---
+++ js3/js/main.js
@@ -0,0 +1,7 @@
+// strings
+const myVariable = "Mathematics"; //not using keyword let beacuse we don't want to reassign the value of variable
+
+// The length property
+
+console.log(myVariable.length); // length property return the number of characters in a string
+
