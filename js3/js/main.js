0b559e08d9ef9a941ccdaec6cfd3a5ed26e08232
Akrati Bhadauria <122599551+Akrati00@users.noreply.github.com>
2024-10-15 16:48:22 +0000

    .

--- js3/js/main.js
+++ js3/js/main.js
@@ -2,6 +2,16 @@
 const myVariable = "Mathematics"; //not using keyword let beacuse we don't want to reassign the value of variable
 
 // The length property
-
 console.log(myVariable.length); // length property return the number of characters in a string
+console.log("Every good boy does fine".length);
+
+// String Methods
+console.log(myVariable.charAt(0));
+
+console.log(myVariable.indexOf("Math")); // 0
+console.log(myVariable.indexOf("mat")); // 5
+
+console.log(myVariable.lastIndexOf("at")); // 6 
+console.log(myVariable.lastIndexOf("ath")); // 1
+
 
