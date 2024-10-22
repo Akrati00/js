47d1e18451fb7e103cc4774efa320feeb560bc6b
Akrati Bhadauria <122599551+Akrati00@users.noreply.github.com>
2024-10-23 02:46:28 +0530

    Create main.js

---
+++ js6/js/main.js
@@ -0,0 +1,22 @@
+// Your First Coding Challenge
+
+// Write code that will return a random letter from your name
+
+console.log("Akrati".charAt(Math.floor(Math.random() * 5 + 1)));
+console.log("Akrati".charAt(Math.floor(Math.random() * 6)));
+// console.log("Akrati".charAt(Math.floor(Math.random() * 5 + 1)));
+// console.log("Akrati".charAt(Math.floor(Math.random() * 5 + 1)));
+// console.log("Akrati".charAt(Math.floor(Math.random() * 5 + 1)));
+// console.log("Akrati".charAt(Math.floor(Math.random() * 5 + 1)));
+// console.log("Akrati".charAt(Math.floor(Math.random() * 5 + 1)));
+// console.log("Akrati".charAt(Math.floor(Math.random() * 5 + 1)));
+// console.log("Akrati".charAt(Math.floor(Math.random() * 5 + 1)));
+// console.log("Akrati".charAt(Math.floor(Math.random() * 5 + 1)));
+
+
+// console.log(Math.floor(0)); // 0 
+
+// random letter from any name
+const mystring = "Akanksha"
+console.log(mystring.charAt(Math.floor(Math.random() * (mystring.length))));
+ 
