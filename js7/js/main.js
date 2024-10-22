b9815492a1b9b5ef1f0b85a211b20019d46dee1d
Akrati Bhadauria <122599551+Akrati00@users.noreply.github.com>
2024-10-23 03:31:09 +0530

    Create main.js

---
+++ js7/js/main.js
@@ -0,0 +1,23 @@
+// conditionals: If Statements
+
+// syntax
+
+/* if(condition){
+    run some code
+} else {
+    run some code
+} */
+
+// Example
+
+// let soup = "corn soup";
+let soup;
+let reply;
+if (soup){
+     reply = `Here's your order of ${soup}`;
+} else{
+    reply = "Sorry, we're out of soup.";
+}
+
+console.log(reply);
+ 
